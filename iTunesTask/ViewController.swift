ac46512924760df8768e770656a8ed0cb7a344c9
FazilKhan7 <200107055@stu.sdu.edu.kz>
2023-06-23 15:44:59 +0600

    Initial Commit

---
+++ iTunesTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesTask
+//
+//  Created by Bakhtiyarov Fozilkhon on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
